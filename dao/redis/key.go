b6b6b4907593a1daa41dfb1eee062483d1e364df
coderpwh <plmoknpwh@163.com>
2023-03-02 09:26:41 +0800

    1.更换redis 2.恢复redis key pre

--- dao/redis/key.go
+++ dao/redis/key.go
@@ -1,10 +1,10 @@
 package redis
 
 const (
-	CaptchaKey          = "go_captcha"
-	UserPre             = "go_User:"
-	GrantedAuthorityPre = "go_GrantedAuthority:"
-	DeptPre             = "go_DeptId:"
-	ChinaData           = "go_ChinaData"
-	NoticeKey           = "go_SystemNotice"
+	CaptchaKey          = "captcha"
+	UserPre             = "User:"
+	GrantedAuthorityPre = "GrantedAuthority:"
+	DeptPre             = "DeptId:"
+	ChinaData           = "ChinaData"
+	NoticeKey           = "SystemNotice"
 )
